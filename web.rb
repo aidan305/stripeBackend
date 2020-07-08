f7594a2d5a8ea2766a146b1f24afb72b542208fd
Aidan <58227901+aidan305@users.noreply.github.com>
2020-07-08 21:11:53 +0100

    Update web.rb

--- web.rb
+++ web.rb
@@ -55,7 +55,7 @@ def authenticate!
       @customer = Stripe::Customer.retrieve(default_customer_id)
     else
       begin
-        @customer = create_customer()
+        @customer = create_customer(params["name"])
 
         if (Stripe.api_key.start_with?('sk_test_'))
           # only attach test cards in testmode
@@ -72,6 +72,7 @@ end
 def create_customer
   Stripe::Customer.create(
     :description => 'mobile SDK example customer',
+    :name => name, 
     :metadata => {
       # Add our application's customer id for this Customer, so it'll be easier to look up
       :my_customer_id => '72F8C533-FCD5-47A6-A45B-3956CA8C792D',
